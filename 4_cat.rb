e40ee01fed1f2448fec255d376a6860327dae3b6
codeamac03 <codeamac03@gmail.com>
2016-11-03 16:53:15 -0600

    done

---
+++ 4_cat.rb
@@ -0,0 +1,26 @@
+# gato = [ ["X", "X", "O"],
+#          ["O", "X", "O"],
+#          ["O", "O", "X"] ]
+
+# p gato[1][2]
+
+def gato
+  hash = { 4 => "X", 5 =>"O"}
+  engine = 9.times.map { rand(4..5) }
+  cat = []
+  engine.each do |position|
+    hash.each_pair { |key, value| 
+      if (position == key) == true
+        cat << "#{value}"
+      end
+    }
+  end
+  cat.each_slice(3).to_a
+end
+
+p gato
+
+10.times do |variable|
+    raise "Not good" if gato == gato
+end
+p true
