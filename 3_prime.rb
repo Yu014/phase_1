f0a99b210416d9cd15db62a5859f921a71704ba9
codeamac03 <codeamac03@gmail.com>
2016-11-07 19:23:02 -0600

    doing the pseudocode

--- 3_prime.rb
+++ 3_prime.rb
@@ -2,15 +2,26 @@
 # array con la combinación de números primos que lo componen.
 # Escribe pseudocódigo antes de empezar con el código.
 
-def prime_factors(num)
-  require 'prime'
-  primes = Prime.first(26)
-  arr = []
-  if num % primes[0] == 0
-    arr << primes[0]
-  end
-  p arr
-end
+Definir el método prime_factors, que recibe un número entero como variable.
+  Requerir el método 'prime' de ruby
+  Asignar un nombre al array que contendrá los (x) primeros números primos
+  Definir un array vacío
+  # IF el número es divisible entre el 1er elemento del array con un residuo de 0
+  #   agrega el 1er elemento del array al array vacío
+  # ELSIF el número es divisible entre el 2do elemento del array con un residuo de 0
+  #   agrega el 2do elemento del array al array vacío
+  Itera sobre arreglo de numeros primos
+
+
+# def prime_factors(num)
+#   require 'prime'
+#   primes = Prime.first(26)
+#   arr = []
+#   if num % primes[0] == 0
+#     arr << primes[0]
+#   end
+#   p arr
+# end
 
 
 
