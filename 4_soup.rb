82991bfe07b932e95932a5ca539be0f6c2fd5590
codeamac03 <codeamac03@gmail.com>
2016-11-11 12:10:32 -0600

    wordking engines for horizontal and vertical search

--- 4_soup.rb
+++ 4_soup.rb
@@ -55,17 +55,62 @@ class Board
     end
   end
 
-  # def vertical
-  #   p word
-  # end
+  def vertical
+    fix_ind = 0 
+    var_ind = 0
+    words = @@boards_templates[0].length
+
+    while var_ind < words
+      word = @@boards_templates[fix_ind][var_ind]
+      var_ind = var_ind + 1
+
+      master = complete_board!.each_slice(5).to_a.transpose
+      indx = 0
+      column = 0
+      elements = 4
+
+      while column <= elements
+        if master[indx].join("").include?(word) == true 
+        p "The word #{word} was found vertically"
+        elsif master[indx].join("").reverse.include?(word) == true 
+        p "The word #{word} was found vertically"
+        end
+        column = column += 1
+        indx = indx += 1
+      end
+
+      # master = complete_board!.each_slice(5).to_a
+      # fila = 0
+      # columna = 1
+      # filas = master.length - 1
+      # columnas = master[0].length - 1
+      
+      # # while columna <= columnas
+      #   something = ""
+      #   while fila <= filas
+      #     something << master[fila][columna]
+      #     fila = fila += 1
+      #   end
+      #   p something
+      #   if something.include?("VERDE") == true
+      #     p "The word #{word} was found vertically"
+      #   elsif something.reverse.include?("VERDE") == true
+      #     p "The word VERDE was found vertically"
+      #   else
+      #     p "Not in here" 
+      #   end
+      #   columna = columna += 1
+      # end
+    end
+  end
 
   # def diagonal
   #   p word
   # end
 
-  def include?(word)
-    p word
-  end
+  # def include?(word)
+  #   p word
+  # end
 
   private
 
@@ -78,4 +123,5 @@ board = Board.new
 # puts board.to_s
 # puts board.word
 board.horizontal
+board.vertical
 # puts board.include?("POEMA")
