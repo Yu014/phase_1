c4ed7ccb23d1e6b92cd83d3b010a9ec415876b68
codeamac03 <codeamac03@gmail.com>
2016-11-15 11:01:12 -0600

    public and private methods

---
+++ 6_pubvspriv.rb
@@ -0,0 +1,54 @@
+class Employee
+  attr_accessor :name, :email, :deposit_account
+  attr_reader :vacation_days, :bonus, :to_s
+  attr_writer :salary
+
+  def initialize(name, email, salary, deposit_account)
+    @name = name
+    @email = email
+    @salary = salary
+    @deposit_account = deposit_account
+    coefficient
+  end
+
+  def deposit_account
+    @deposit_account.gsub!("-", "")
+    "*****#{@deposit_account[-4..-1]}" 
+  end
+
+  def to_s
+    p "#{@name} <email: #{@email}> acct: #{deposit_account}"
+  end
+
+  def vacation_days
+    coefficient * 7  
+  end
+
+  def bonus
+    coefficient * 1000
+  end
+
+  private
+
+  def salary
+  end
+
+  def coefficient
+    coefficients = { 1 => 0...1_000, 1.2 => 1_000...2_000, 1.4 => 2_000...5_000, 1.5 => 5_000..10_000 }
+    coefficients.find { |coefficient, range| range.include? @salary }.first
+  end
+end
+
+
+employee = Employee.new('Juan Perez', 'juan@gmail.com', 1_800, '123-456-512')
+employee.to_s
+# => "Juan Perez <email: juan@gmail.com> acct: *****6512"
+
+str = "The employee information is #{employee}"
+puts str
+# => "The employee information is Juan Perez <email: juan@gmail.com> acct: *****6512"
+
+puts str == "The employee information is Juan Perez <email: juan@gmail.com> acct: *****6512"
+
+puts employee.vacation_days == 8.4
+puts employee.bonus == 1_200
