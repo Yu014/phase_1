308feca8839778b2282ed39cec6e2533bfadec0a
codeamac03 <codeamac03@gmail.com>
2016-11-15 14:37:01 -0600

    starting hen

--- 6_hen.rb
+++ 6_hen.rb
@@ -1,14 +1,22 @@
 class LayingHen
 
   def initialize
+    @age = 0
+    # @eggs = []
   end
 
   # Ages the hen one month, and lays 4 eggs if the hen is older than 3 months
   def age!
+    @age += 1
+    if @age >= 3
+      any_eggs? = 4
+    end
+    #this relates to Egg class
   end
 
   # Returns +true+ if the hen has laid any eggs, +false+ otherwise
   def any_eggs?
+
   end
 
   # Returns an Egg if there are any
@@ -28,56 +36,57 @@ class LayingHen
   end
 end
 
-class Egg
-  # Initializes a new Egg with hatched hours +hatched_hours+
-  def initialize
-  end
+# class Egg
+#   # Initializes a new Egg with hatched hours +hatched_hours+
+#   def initialize
+#   end
 
-  # Return +true+ if hatched hours is greater than 3, +false+ otherwise
-  def rotten?
-  end
-end
+#   # Return +true+ if hatched hours is greater than 3, +false+ otherwise
+#   def rotten?
+#   end
+# end
 
 
 hen = LayingHen.new
 basket = []
 rotten_eggs = 0
 
-hen.age! until hen.any_eggs?
+p hen.age! #until hen.any_eggs?
+# hen.eggs
 
-puts "Hen is #{hen.age} months old, its starting to laid eggs."
-puts ""
+# puts "Hen is #{hen.age} months old, its starting to laid eggs."
+# puts ""
 
-until hen.old?
+# until hen.old?
 
-  # The time we take to pick up the eggs
-  hours = rand(5)
-  hen.increase_hatched_hour(hours)
+#   # The time we take to pick up the eggs
+#   hours = rand(5)
+#   hen.increase_hatched_hour(hours)
 
-  while hen.any_eggs?
-    egg = hen.pick_an_egg!
+#   while hen.any_eggs?
+#     egg = hen.pick_an_egg!
 
-    if egg.rotten?
-      rotten_eggs += 1
-    else
-      basket << egg
-    end
-  end
+#     if egg.rotten?
+#       rotten_eggs += 1
+#     else
+#       basket << egg
+#     end
+#   end
 
-  puts "Month #{hen.age} Report"
-  puts "We took #{hours} hour(s) to pick the eggs"
-  puts "Eggs in the basket: #{basket.size}"
-  puts "Rotten eggs: #{rotten_eggs}"
-  puts ""
+#   puts "Month #{hen.age} Report"
+#   puts "We took #{hours} hour(s) to pick the eggs"
+#   puts "Eggs in the basket: #{basket.size}"
+#   puts "Rotten eggs: #{rotten_eggs}"
+#   puts ""
 
-  # Age the hen another month
-  hen.age!
-end
+#   # Age the hen another month
+#   hen.age!
+# end
 
-puts "The hen is old, she can't laid more eggs!"
-puts "The hen laid #{basket.size + rotten_eggs} eggs"
-if rotten_eggs == 0
-  puts "CONGRATULATIONS NO ROTTEN EGGS" 
-else
-  puts "We pick to late #{rotten_eggs} eggs so they become rotten"
-end
+# puts "The hen is old, she can't laid more eggs!"
+# puts "The hen laid #{basket.size + rotten_eggs} eggs"
+# if rotten_eggs == 0
+#   puts "CONGRATULATIONS NO ROTTEN EGGS" 
+# else
+#   puts "We pick too late #{rotten_eggs} eggs so they become rotten"
+# end
