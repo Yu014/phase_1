959745d720c8ea4cb381ad7465061942e98ae1a6
codeamac03 <codeamac03@gmail.com>
2016-11-16 14:40:12 -0600

    inheritance working

---
+++ 7_vehicle.rb
@@ -0,0 +1,92 @@
+class Vehicle
+  attr_reader :age
+  def what_am_i?
+    # Este método va a ser utilizado por varias clases y deberá regresar 
+    # el nombre de la clase desde la cual se corrió.
+    # ej.
+    # bocho = Car.new
+    # bocho.what_am_i? => Car
+  end
+  def number_of_wheels
+    4
+  end
+  def color
+    "color"
+  end
+  def age!
+    "age"
+  end
+end
+
+class Motorized < Vehicle
+  def has_motor
+    true
+  end
+  def tank_size
+    "big"
+  end
+  def refuel
+    "needed"
+  end
+  def number_of_gears
+    "number"
+  end
+end
+
+class Motorbike < Motorized
+  def age
+    "9 years"
+  end
+end
+
+class Car < Motorized
+  def age
+    "2 years"
+  end
+end
+
+class Bicycle < Motorized
+  def initialize(num)
+    @num = num
+  end
+  def has_motor
+    false
+  end
+  def number_of_gears(num)
+    num
+  end
+  def age
+    "4 years"
+  end
+end
+
+class Skateboard < Motorized
+  def has_motor
+    false
+  end
+  def age
+    "6 months"
+  end
+end
+
+moto = Motorbike.new
+car = Car.new
+bike = Bicycle.new(8)
+skateboard = Skateboard.new
+
+vehicles = [moto, car, bike, skateboard]
+
+
+vehicles.each do |vehicle|
+  vehicle.age!
+  puts "#{vehicle.what_am_i?} responds to:"
+  puts "\tNumber of wheels: #{vehicle.respond_to?(:number_of_wheels) == true}"
+  puts "\tColor: #{vehicle.respond_to?(:color) == true}"
+  puts "\tAge!: #{vehicle.respond_to?(:age!) == true}"
+  puts "\tHas motor: #{vehicle.respond_to?(:has_motor) == true}"
+  puts "\tTank size: #{vehicle.respond_to?(:tank_size) == true}"
+  puts "\tRefuel: #{vehicle.respond_to?(:refuel) == true}"
+  puts "\tNumber of gears: #{vehicle.respond_to?(:number_of_gears) == true}"
+  puts "\tAge: #{vehicle.age}"
+  puts "\n\n"
+end
