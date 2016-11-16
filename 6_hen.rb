d338b26c247c762b49c9ea5f2d8dbee408c0b0f6
codeamac03 <codeamac03@gmail.com>
2016-11-16 11:54:51 -0600

    finished method for gallinita ponedora

--- 6_hen.rb
+++ 6_hen.rb
@@ -1,22 +1,27 @@
+$hours
+
 class LayingHen
+  attr_reader :age, :eggs 
 
   def initialize
     @age = 0
-    # @eggs = []
+    @eggs = []
   end
 
   # Ages the hen one month, and lays 4 eggs if the hen is older than 3 months
   def age!
     @age += 1
-    if @age >= 3
-      any_eggs? = 4
+    if @age > 3
+      4.times do 
+        @eggs << Egg.new
+      end
     end
     #this relates to Egg class
   end
 
   # Returns +true+ if the hen has laid any eggs, +false+ otherwise
   def any_eggs?
-
+    @eggs.length != 0 ? true : false
   end
 
   # Returns an Egg if there are any
@@ -25,68 +30,72 @@ class LayingHen
     raise NoEggsError, "The hen has not layed any eggs" unless self.any_eggs?
 
     # egg-picking logic goes here
+    @eggs.pop
   end
 
   # Returns +true+ if the hen can't laid eggs anymore because of its age, +false+ otherwise.
   # The max age for a hen to lay eggs is 10 
   def old?
+    @age >= 10 ? true : false
   end
 
   def increase_hatched_hour(hours)
+    $hours = hours
   end
 end
 
-# class Egg
-#   # Initializes a new Egg with hatched hours +hatched_hours+
-#   def initialize
-#   end
+class Egg 
+  # Initializes a new Egg with hatched hours +hatched_hours+
+  def initialize
+    $hours
+  end
 
-#   # Return +true+ if hatched hours is greater than 3, +false+ otherwise
-#   def rotten?
-#   end
-# end
+  # Return +true+ if hatched hours is greater than 3, +false+ otherwise
+  def rotten?
+    $hours > 3 ? true : false
+  end
+end
 
 
 hen = LayingHen.new
 basket = []
 rotten_eggs = 0
 
-p hen.age! #until hen.any_eggs?
-# hen.eggs
+hen.age! until hen.any_eggs?
 
-# puts "Hen is #{hen.age} months old, its starting to laid eggs."
-# puts ""
+puts "Hen is #{hen.age} months old, its starting to laid eggs."
+puts ""
 
-# until hen.old?
+until hen.old?
 
-#   # The time we take to pick up the eggs
-#   hours = rand(5)
-#   hen.increase_hatched_hour(hours)
+  # The time we take to pick up the eggs
+  hours = rand(5)
+  hen.increase_hatched_hour(hours)
 
-#   while hen.any_eggs?
-#     egg = hen.pick_an_egg!
+  while hen.any_eggs?
+    egg = hen.pick_an_egg!
+    
+    if egg.rotten?
+      rotten_eggs += 1
+    else
+      basket << egg
+    end
+  end
 
-#     if egg.rotten?
-#       rotten_eggs += 1
-#     else
-#       basket << egg
-#     end
-#   end
+  puts "Month #{hen.age} Report"
+  puts "We took #{hours} hour(s) to pick the eggs"
+  puts "Eggs in the basket: #{basket.size}"
+  puts "Rotten eggs: #{rotten_eggs}"
+  puts ""
 
-#   puts "Month #{hen.age} Report"
-#   puts "We took #{hours} hour(s) to pick the eggs"
-#   puts "Eggs in the basket: #{basket.size}"
-#   puts "Rotten eggs: #{rotten_eggs}"
-#   puts ""
+  # Age the hen another month
+  hen.age!
+end
 
-#   # Age the hen another month
-#   hen.age!
-# end
-
-# puts "The hen is old, she can't laid more eggs!"
-# puts "The hen laid #{basket.size + rotten_eggs} eggs"
-# if rotten_eggs == 0
-#   puts "CONGRATULATIONS NO ROTTEN EGGS" 
-# else
-#   puts "We pick too late #{rotten_eggs} eggs so they become rotten"
-# end
+puts "The hen is old, she can't laid more eggs!"
+puts "The hen laid #{basket.size + rotten_eggs} eggs"
+if rotten_eggs == 0
+  puts "CONGRATULATIONS NO ROTTEN EGGS" 
+else
+  puts "We pick too late #{rotten_eggs} eggs so they become rotten"
+end
